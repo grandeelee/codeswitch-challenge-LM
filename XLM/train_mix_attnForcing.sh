3147c866665da3d75bb7c9a53807045f4584cfc9
grandeelee <grandee.lee@u.nus.edu>
2019-11-09 00:51:33 +0800

    minor

--- XLM/train_mix_attnForcing.sh
+++ XLM/train_mix_attnForcing.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-python train_adapt.py --gpus 1 --model ../save/xlm_baseline_mix_attn --nlayers 12 --attn_forcing decreasing
+python train_adapt.py --gpus 3 --model ../save/xlm_baseline_mix_attn --nlayers 12 --attn_forcing increasing --batch_size 30
