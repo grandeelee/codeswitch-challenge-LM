8eb210937a39c51dc62ce45d5c0a36cd03d62af3
grandeelee <grandee.lee@u.nus.edu>
2019-11-10 20:16:52 +0800

    attn_forcing

--- XLM/train_mix_attnForcing.sh
+++ XLM/train_mix_attnForcing.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python train_adapt.py --gpus 3 --model ../save/xlm_baseline_mix_attn_decrease_schedule --nlayers 12 \
-                      --attn_forcing decreasing --batch_size 40
+python train_adapt.py --gpus 2 --model ../save/xlm_baseline_mix_attn_constant_schedule --nlayers 12 \
+                      --attn_forcing constant --batch_size 40
