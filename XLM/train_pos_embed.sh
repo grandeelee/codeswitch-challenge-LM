4f8ab9dcef7d640efe7abc73c951a272374e5a97
grandeelee <grandee.lee@u.nus.edu>
2019-11-18 16:54:28 +0800

    minor

--- XLM/train_pos_embed.sh
+++ XLM/train_pos_embed.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python train_adapt.py --gpus 3 --model ../save/xlm_mix_attn_constant_pos_embed --nlayers 12 \
-                      --attn_forcing constant --batch_size 50 --epochs 60 --adapt_epochs 17 \
+python train_adapt.py --gpus 1 --model ../save/xlm_mix_attn_no_pos_embed --nlayers 12 \
+                      --attn_forcing None --batch_size 50 --epochs 60 --adapt_epochs 17 \
                       --pos_embed
