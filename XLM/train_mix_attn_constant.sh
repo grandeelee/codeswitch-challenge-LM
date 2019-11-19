1efd3fa80b44aa90ec30d1058019902b2c3be8dc
grandeelee <grandee.lee@u.nus.edu>
2019-11-19 16:32:47 +0800

    minor

--- XLM/train_mix_attn_constant.sh
+++ XLM/train_mix_attn_constant.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python train_adapt.py --gpus 3 --model ../save/xlm_baseline_mix_attn_constant_schedule --nlayers 12 \
-                      --attn_forcing constant --batch_size 50 --epochs 60 --adapt_epochs 17
+                      --attn_forcing constant --batch_size 50 --epochs 60 --adapt_epochs 17 --resume_train 9
