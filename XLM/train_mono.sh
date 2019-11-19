1fd974d869369ae7d7c32a03809229209754a452
grandeelee <grandee.lee@u.nus.edu>
2019-11-19 16:36:22 +0800

    minor

--- XLM/train_mono.sh
+++ XLM/train_mono.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python train_mono.py --gpus 1 --model ../save/mono_only --nlayers 12 \
+python train_mono.py --gpus 0 --model ../save/mono_only --nlayers 12 \
                       --attn_forcing None --batch_size 50 --epochs 60 --adapt_epochs 17 \
-                      --max_len 68 --data ../data/cs_para/
+                      --max_len 68 --data ../data/cs_para/ --resume_train 17
