b7a641c883fc12864b25aeb4868c38e32cbd5add
grandeelee <grandee.lee@u.nus.edu>
2019-11-19 16:29:14 +0800

    minor

--- XLM/train_pos_embed.sh
+++ XLM/train_pos_embed.sh
@@ -2,4 +2,4 @@
 
 python train_adapt.py --gpus 2 --model ../save/xlm_mix_attn_no_pos_embed --nlayers 12 \
                       --attn_forcing None --batch_size 50 --epochs 60 --adapt_epochs 17 \
-                      --pos_embed
+                      --pos_embed --resume_train 16
--- XLM/train_sin_embed.sh
+++ XLM/train_sin_embed.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python train_adapt.py --gpus 0 --model ../save/xlm_mix_attn_no_sin_embed --nlayers 12 \
+python train_adapt.py --gpus 1 --model ../save/xlm_mix_attn_no_sin_embed --nlayers 12 \
                       --attn_forcing None --batch_size 50 --epochs 60 --adapt_epochs 17 \
-                      --sin_embed --resume_train 6
+                      --sin_embed --resume_train 22
