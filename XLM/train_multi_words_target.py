6f6e528ebb785d2d852c178e69fd327c43f14b41
grandeelee <grandee.lee@u.nus.edu>
2019-11-29 13:55:42 +0800

    minor

--- XLM/train_multi_words_target.py
+++ XLM/train_multi_words_target.py
@@ -97,10 +97,8 @@ def evaluate(generator):
         pred_mask = alen[None] < lengths[:, None] - 1
         if args.pos_embed or args.sin_embed:
             y = x[:, 1:, 0].masked_select(pred_mask[:, :-1])
-            y_1 = torch.cat((x[:, 2:, 0], x[:, 0, 0])).masked_select(pred_mask[:, :-1])
         else:
             y = x[:, 1:].masked_select(pred_mask[:, :-1])
-            y_1 = torch.cat((x[:, 2:], x[:, 0])).masked_select(pred_mask[:, :-1])
 
         x = x.to(device)
         pred_mask = pred_mask.to(device)
@@ -137,10 +135,10 @@ def run_epoch():
             # select target to be first word until eos
             if args.pos_embed or args.sin_embed:
                 y = x[:, 1:, 0].masked_select(pred_mask[:, :-1])
-                y_1 = torch.cat((x[:, 2:, 0], x[:, 0, 0])).masked_select(pred_mask[:, :-1])
+                y_1 = torch.cat((x[:, 2:, 0], x[:, 0, 0]), dim=-1).masked_select(pred_mask[:, :-1])
             else:
                 y = x[:, 1:].masked_select(pred_mask[:, :-1])
-                y_1 = torch.cat((x[:, 2:], x[:, 0])).masked_select(pred_mask[:, :-1])
+                y_1 = torch.cat((x[:, 2:], x[:, 0]), dim=-1).masked_select(pred_mask[:, :-1])
             assert pred_mask.sum().item() == y_1.size(0)
 
             x = x.to(device)
@@ -174,8 +172,10 @@ def run_epoch():
             # select target to be first word until eos
             if args.pos_embed or args.sin_embed:
                 y = x[:, 1:, 0].masked_select(pred_mask[:, :-1])
+                y_1 = torch.cat((x[:, 2:, 0], x[:, 0, 0]), dim=-1).masked_select(pred_mask[:, :-1])
             else:
                 y = x[:, 1:].masked_select(pred_mask[:, :-1])
+                y_1 = torch.cat((x[:, 2:], x[:, 0]), dim=-1).masked_select(pred_mask[:, :-1])
             assert pred_mask.sum().item() == y.size(0)
 
             x = x.to(device)
