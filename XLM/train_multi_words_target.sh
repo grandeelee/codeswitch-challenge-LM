42c9d7e5a4b5cd46286cd0277b6da57363957dd5
grandeelee <grandee.lee@u.nus.edu>
2019-11-29 13:03:06 +0800

    minor

---
+++ XLM/train_multi_words_target.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+python train_multi_words_target.py --gpus 1 --model ../save/mix_multi_words_target --nlayers 12 \
+                      --attn_forcing None --batch_size 50 --epochs 60 --adapt_epochs 17 \
+                      --max_len 68 --data ../data/cs_para/
